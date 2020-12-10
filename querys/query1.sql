75e5b6f5bb17a57a4e79ed42f57264d5ab8cb817
renatoseb <renatorodriguezllanos@gmail.com>
2020-12-10 02:10:26 -0500

    :alien: querys done

--- querys/query1.sql
+++ querys/query1.sql
@@ -4,6 +4,7 @@ Justificación: A veces llegan a atenderse clientes que
 informan que son clientes frecuentes y se verifica. Si 
 son clientes frecuentes se le ofrece un trato diferente.*/
 
+-- mil
 SELECT C.nombres,C.apellidos
     FROM mil.cliente C, (
     SELECT DNI_cliente, COUNT(DNI_cliente) AS total
@@ -28,3 +29,134 @@ SELECT C.nombres,C.apellidos
     ORDER BY total DESC
 ) RB
 WHERE RB.DNI_cliente = C.DNI;
+
+-- mil_i
+SELECT C.nombres,C.apellidos
+    FROM mil_i.cliente C, (
+    SELECT DNI_cliente, COUNT(DNI_cliente) AS total
+    FROM (SELECT DNI_cliente 
+        FROM  mil_i.Realiza_Boleta
+        UNION ALL
+        SELECT DNI_cliente
+        FROM mil_i.Realiza_Factura) S
+    GROUP BY(DNI_cliente)
+    HAVING COUNT(DNI_cliente) =(
+        SELECT MAX(total)
+        FROM(
+            SELECT DNI_cliente, COUNT(DNI_cliente) AS total
+            FROM (SELECT DNI_cliente 
+                FROM  mil_i.Realiza_Boleta
+                UNION ALL
+                SELECT DNI_cliente
+                FROM mil_i.Realiza_Factura) S
+            GROUP BY(DNI_cliente)
+        ) C
+    )
+    ORDER BY total DESC
+) RB
+WHERE RB.DNI_cliente = C.DNI;
+
+-- mil_10
+SELECT C.nombres,C.apellidos
+    FROM mil_10.cliente C, (
+    SELECT DNI_cliente, COUNT(DNI_cliente) AS total
+    FROM (SELECT DNI_cliente 
+        FROM  mil_10.Realiza_Boleta
+        UNION ALL
+        SELECT DNI_cliente
+        FROM mil_10.Realiza_Factura) S
+    GROUP BY(DNI_cliente)
+    HAVING COUNT(DNI_cliente) =(
+        SELECT MAX(total)
+        FROM(
+            SELECT DNI_cliente, COUNT(DNI_cliente) AS total
+            FROM (SELECT DNI_cliente 
+                FROM  mil_10.Realiza_Boleta
+                UNION ALL
+                SELECT DNI_cliente
+                FROM mil_10.Realiza_Factura) S
+            GROUP BY(DNI_cliente)
+        ) C
+    )
+    ORDER BY total DESC
+) RB
+WHERE RB.DNI_cliente = C.DNI;
+
+-- mil_10_i
+SELECT C.nombres,C.apellidos
+    FROM mil_10_i.cliente C, (
+    SELECT DNI_cliente, COUNT(DNI_cliente) AS total
+    FROM (SELECT DNI_cliente 
+        FROM  mil_10_i.Realiza_Boleta
+        UNION ALL
+        SELECT DNI_cliente
+        FROM mil_10_i.Realiza_Factura) S
+    GROUP BY(DNI_cliente)
+    HAVING COUNT(DNI_cliente) =(
+        SELECT MAX(total)
+        FROM(
+            SELECT DNI_cliente, COUNT(DNI_cliente) AS total
+            FROM (SELECT DNI_cliente 
+                FROM  mil_10_i.Realiza_Boleta
+                UNION ALL
+                SELECT DNI_cliente
+                FROM mil_10_i.Realiza_Factura) S
+            GROUP BY(DNI_cliente)
+        ) C
+    )
+    ORDER BY total DESC
+) RB
+WHERE RB.DNI_cliente = C.DNI;
+
+-- mil_100
+SELECT C.nombres,C.apellidos
+    FROM mil_100.cliente C, (
+    SELECT DNI_cliente, COUNT(DNI_cliente) AS total
+    FROM (SELECT DNI_cliente 
+        FROM  mil_100.Realiza_Boleta
+        UNION ALL
+        SELECT DNI_cliente
+        FROM mil_100.Realiza_Factura) S
+    GROUP BY(DNI_cliente)
+    HAVING COUNT(DNI_cliente) =(
+        SELECT MAX(total)
+        FROM(
+            SELECT DNI_cliente, COUNT(DNI_cliente) AS total
+            FROM (SELECT DNI_cliente 
+                FROM  mil_100.Realiza_Boleta
+                UNION ALL
+                SELECT DNI_cliente
+                FROM mil_100.Realiza_Factura) S
+            GROUP BY(DNI_cliente)
+        ) C
+    )
+    ORDER BY total DESC
+) RB
+WHERE RB.DNI_cliente = C.DNI;
+
+-- mil_100_i
+SELECT C.nombres,C.apellidos
+    FROM mil_100_i.cliente C, (
+    SELECT DNI_cliente, COUNT(DNI_cliente) AS total
+    FROM (SELECT DNI_cliente 
+        FROM  mil_100_i.Realiza_Boleta
+        UNION ALL
+        SELECT DNI_cliente
+        FROM mil_100_i.Realiza_Factura) S
+    GROUP BY(DNI_cliente)
+    HAVING COUNT(DNI_cliente) =(
+        SELECT MAX(total)
+        FROM(
+            SELECT DNI_cliente, COUNT(DNI_cliente) AS total
+            FROM (SELECT DNI_cliente 
+                FROM  mil_100_i.Realiza_Boleta
+                UNION ALL
+                SELECT DNI_cliente
+                FROM mil_100_i.Realiza_Factura) S
+            GROUP BY(DNI_cliente)
+        ) C
+    )
+    ORDER BY total DESC
+) RB
+WHERE RB.DNI_cliente = C.DNI;
+
--- querys/query2.sql
+++ querys/query2.sql
@@ -17,6 +17,7 @@ LIMIT 1;
 --¿4. ¿Cúal fue el local que más vendío en un determinado mes y año?
 /*Locales que han generado una mayor ganancia en un tiempo determinado.
 Justificación: A veces el propietario de la empresa desea verificar las ganancias  de cada local y cual de ellos está obtiendo mayores ingreso en el transcurso del mes.*/
+-- mil
 SELECT Direccion, SUM(monto) as total
 FROM(
     SELECT T.Direccion as Direccion, B.monto as monto
@@ -26,6 +27,91 @@ FROM(
     UNION ALL
     SELECT T.Direccion as Direccion, F.monto as monto
     FROM mil.Factura F, mil.Terapista T, mil.Realiza_Factura RF
+    WHERE F.DNI = RF.DNI_cliente AND T.DNI = RF.DNI_terapista 
+    AND EXTRACT('MONTH' FROM F.fecha) = 2 AND EXTRACT('YEAR' FROM F.fecha) = 2020
+    ) T
+GROUP BY (Direccion)
+ORDER BY total DESC
+LIMIT 1;
+
+-- mil_i
+SELECT Direccion, SUM(monto) as total
+FROM(
+    SELECT T.Direccion as Direccion, B.monto as monto
+    FROM mil_i.Boleta B, mil_i.Terapista T, mil_i.Realiza_Boleta RB
+    WHERE B.DNI = RB.DNI_cliente AND T.DNI = RB.DNI_terapista 
+    AND EXTRACT('MONTH' FROM B.fecha) = 2 AND EXTRACT('YEAR' FROM B.fecha) = 2020
+    UNION ALL
+    SELECT T.Direccion as Direccion, F.monto as monto
+    FROM mil_i.Factura F, mil_i.Terapista T, mil_i.Realiza_Factura RF
+    WHERE F.DNI = RF.DNI_cliente AND T.DNI = RF.DNI_terapista 
+    AND EXTRACT('MONTH' FROM F.fecha) = 2 AND EXTRACT('YEAR' FROM F.fecha) = 2020
+    ) T
+GROUP BY (Direccion)
+ORDER BY total DESC
+LIMIT 1;
+
+-- mil_10
+SELECT Direccion, SUM(monto) as total
+FROM(
+    SELECT T.Direccion as Direccion, B.monto as monto
+    FROM mil_10.Boleta B, mil_10.Terapista T, mil_10.Realiza_Boleta RB
+    WHERE B.DNI = RB.DNI_cliente AND T.DNI = RB.DNI_terapista 
+    AND EXTRACT('MONTH' FROM B.fecha) = 2 AND EXTRACT('YEAR' FROM B.fecha) = 2020
+    UNION ALL
+    SELECT T.Direccion as Direccion, F.monto as monto
+    FROM mil_10.Factura F, mil_10.Terapista T, mil_10.Realiza_Factura RF
+    WHERE F.DNI = RF.DNI_cliente AND T.DNI = RF.DNI_terapista 
+    AND EXTRACT('MONTH' FROM F.fecha) = 2 AND EXTRACT('YEAR' FROM F.fecha) = 2020
+    ) T
+GROUP BY (Direccion)
+ORDER BY total DESC
+LIMIT 1;
+
+-- mil_10_i
+SELECT Direccion, SUM(monto) as total
+FROM(
+    SELECT T.Direccion as Direccion, B.monto as monto
+    FROM mil_10_i.Boleta B, mil_10_i.Terapista T, mil_10_i.Realiza_Boleta RB
+    WHERE B.DNI = RB.DNI_cliente AND T.DNI = RB.DNI_terapista 
+    AND EXTRACT('MONTH' FROM B.fecha) = 2 AND EXTRACT('YEAR' FROM B.fecha) = 2020
+    UNION ALL
+    SELECT T.Direccion as Direccion, F.monto as monto
+    FROM mil_10_i.Factura F, mil_10_i.Terapista T, mil_10_i.Realiza_Factura RF
+    WHERE F.DNI = RF.DNI_cliente AND T.DNI = RF.DNI_terapista 
+    AND EXTRACT('MONTH' FROM F.fecha) = 2 AND EXTRACT('YEAR' FROM F.fecha) = 2020
+    ) T
+GROUP BY (Direccion)
+ORDER BY total DESC
+LIMIT 1;
+
+-- mil_100
+SELECT Direccion, SUM(monto) as total
+FROM(
+    SELECT T.Direccion as Direccion, B.monto as monto
+    FROM mil_100.Boleta B, mil_100.Terapista T, mil_100.Realiza_Boleta RB
+    WHERE B.DNI = RB.DNI_cliente AND T.DNI = RB.DNI_terapista 
+    AND EXTRACT('MONTH' FROM B.fecha) = 2 AND EXTRACT('YEAR' FROM B.fecha) = 2020
+    UNION ALL
+    SELECT T.Direccion as Direccion, F.monto as monto
+    FROM mil_100.Factura F, mil_100.Terapista T, mil_100.Realiza_Factura RF
+    WHERE F.DNI = RF.DNI_cliente AND T.DNI = RF.DNI_terapista 
+    AND EXTRACT('MONTH' FROM F.fecha) = 2 AND EXTRACT('YEAR' FROM F.fecha) = 2020
+    ) T
+GROUP BY (Direccion)
+ORDER BY total DESC
+LIMIT 1;
+
+-- mil_100_i
+SELECT Direccion, SUM(monto) as total
+FROM(
+    SELECT T.Direccion as Direccion, B.monto as monto
+    FROM mil_100_i.Boleta B, mil_100_i.Terapista T, mil_100_i.Realiza_Boleta RB
+    WHERE B.DNI = RB.DNI_cliente AND T.DNI = RB.DNI_terapista 
+    AND EXTRACT('MONTH' FROM B.fecha) = 2 AND EXTRACT('YEAR' FROM B.fecha) = 2020
+    UNION ALL
+    SELECT T.Direccion as Direccion, F.monto as monto
+    FROM mil_100_i.Factura F, mil_100_i.Terapista T, mil_100_i.Realiza_Factura RF
     WHERE F.DNI = RF.DNI_cliente AND T.DNI = RF.DNI_terapista 
     AND EXTRACT('MONTH' FROM F.fecha) = 2 AND EXTRACT('YEAR' FROM F.fecha) = 2020
     ) T
