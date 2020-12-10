d6a3a2cc69c8b02543b5e2f7ebdb8b7d73de9f8b
renatoseb <renatorodriguezllanos@gmail.com>
2020-12-10 01:31:10 -0500

    :alien: add dump except millon

--- querys/query2.sql
+++ querys/query2.sql
@@ -3,12 +3,11 @@ SELECT Direccion, SUM(monto) as total
 FROM(
     SELECT T.Direccion as Direccion, B.monto as monto
     FROM mil.Boleta B, mil.Terapista T, mil.Realiza_Boleta RB
-    WHERE B.DNI = RB.DNI_cliente AND T.DNI = RB.DNI_terapista AND EXTRACT('MONTH' FROM B.fecha) = 2
-    AND 
+    WHERE B.DNI = RB.DNI_cliente AND T.DNI = RB.DNI_terapista
     UNION ALL
     SELECT T.Direccion as Direccion, F.monto as monto
     FROM mil.Factura F, mil.Terapista T, mil.Realiza_Factura RF
-    WHERE F.DNI = RF.DNI_cliente AND T.DNI = RF.DNI_terapista AND EXTRACT('MONTH' FROM B.fecha) = 2
+    WHERE F.DNI = RF.DNI_cliente AND T.DNI = RF.DNI_terapista
     ) T
 GROUP BY (Direccion)
 ORDER BY total DESC
@@ -22,11 +21,13 @@ SELECT Direccion, SUM(monto) as total
 FROM(
     SELECT T.Direccion as Direccion, B.monto as monto
     FROM mil.Boleta B, mil.Terapista T, mil.Realiza_Boleta RB
-    WHERE B.DNI = RB.DNI_cliente AND T.DNI = RB.DNI_terapista AND EXTRACT('MONTH' FROM B.fecha) = 2 AND EXTRACT('YEAR' FROM B.fecha) = 2020
+    WHERE B.DNI = RB.DNI_cliente AND T.DNI = RB.DNI_terapista 
+    AND EXTRACT('MONTH' FROM B.fecha) = 2 AND EXTRACT('YEAR' FROM B.fecha) = 2020
     UNION ALL
     SELECT T.Direccion as Direccion, F.monto as monto
     FROM mil.Factura F, mil.Terapista T, mil.Realiza_Factura RF
-    WHERE F.DNI = RF.DNI_cliente AND T.DNI = RF.DNI_terapista AND EXTRACT('MONTH' FROM F.fecha) = 2 AND EXTRACT('YEAR' FROM F.fecha) = 2020
+    WHERE F.DNI = RF.DNI_cliente AND T.DNI = RF.DNI_terapista 
+    AND EXTRACT('MONTH' FROM F.fecha) = 2 AND EXTRACT('YEAR' FROM F.fecha) = 2020
     ) T
 GROUP BY (Direccion)
 ORDER BY total DESC
