c0585236e3399b34b08287932555556126d9003e
Andres20-Utec <andres.riveros@utec.edu.pe>
2020-12-09 19:23:16 -0500

    update query1

--- querys/query1.sql
+++ querys/query1.sql
@@ -1,10 +1,26 @@
-SELECT DNI_cliente, COUNT(DNI_cliente) AS total
-FROM (SELECT DNI_cliente 
-	FROM  Realiza_Boleta
-	UNION ALL
-	SELECT DNI_cliente
-	FROM Realiza_Factura) S
-GROUP BY(DNI_cliente)
-ORDER BY total DESC
-LIMIT 3;
+--¿Cúal es el cliente más frecuente?
 
+SELECT C.nombres,C.apellidos
+	FROM mil.cliente C, (
+	SELECT DNI_cliente, COUNT(DNI_cliente) AS total
+	FROM (SELECT DNI_cliente 
+		FROM  mil.Realiza_Boleta
+		UNION ALL
+		SELECT DNI_cliente
+		FROM mil.Realiza_Factura) S
+	GROUP BY(DNI_cliente)
+	HAVING COUNT(DNI_cliente) =(
+		SELECT MAX(total)
+		FROM(
+			SELECT DNI_cliente, COUNT(DNI_cliente) AS total
+			FROM (SELECT DNI_cliente 
+				FROM  mil.Realiza_Boleta
+				UNION ALL
+				SELECT DNI_cliente
+				FROM mil.Realiza_Factura) S
+			GROUP BY(DNI_cliente)
+		) C
+	)
+	ORDER BY total DESC
+) RB
+WHERE RB.DNI_cliente = C.DNI;
